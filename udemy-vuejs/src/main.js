a05353dacc7736cda1a0bb86d78c1aa9bd80fe8e
otegami <a.s.takuya1026@gmail.com>
2021-01-09 21:55:12 +0900

    learn what vue file is

--- udemy-vuejs/src/main.js
+++ udemy-vuejs/src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+// Vue Loader が '.vue' ファイルをシングルコンポーネントファイルとして認識して使うことができる
+// import したら component になるものが、 .vue ファイル
 import App from './App.vue'
 
 Vue.config.productionTip = false
