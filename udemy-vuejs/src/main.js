32a4a550e6b24f0cc53fe1a0526c9d87465b46c0
otegami <a.s.takuya1026@gmail.com>
2021-01-09 22:03:56 +0900

    learn single componnt

--- udemy-vuejs/src/main.js
+++ udemy-vuejs/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 // Vue Loader が '.vue' ファイルをシングルコンポーネントファイルとして認識して使うことができる
 // import したら component になるものが、 .vue ファイル
 import App from './App.vue'
+import LikeNumber from './LikeNumber.vue'
 
 Vue.config.productionTip = false
+Vue.component('LikeNumber', LikeNumber)
 
 new Vue({
   render: h => h(App),
