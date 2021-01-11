909968db97bc321bc69ec9d14a0284923e6cb826
otegami <a.s.takuya1026@gmail.com>
2021-01-11 20:22:13 +0900

    learn filter and mixin
    mixin はプラグインを作成するときなどに使われることが多い

---
+++ udemy-vuejs/src/components/CountNumber.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ title | lowerCase | upperCase }}</h2>
+    <p>{{ subTitle | upperCase | lowerCase }}</p>
+    <p>{{ number }}</p>
+    <button @click="number ++">+1</button>
+  </div>
+</template>
+
+<script>
+import { tokyoNumber } from "@/tokyoNumber"
+export default {
+  mixins: [ tokyoNumber ],
+  created() {
+    console.log("created in component")
+  },
+  data() {
+    return {
+      title: 'L.A.'
+    }
+  }
+}
+</script>
