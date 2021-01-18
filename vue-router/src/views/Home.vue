def0ec28174d76c714c7a5d71e73dea4ec522a84
otegami <a.s.takuya1026@gmail.com>
2021-01-18 22:02:32 +0900

    learn about  &

--- vue-router/src/views/Home.vue
+++ vue-router/src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
     <h1>Home</h1>
+    <button @click="toUsers">Users のページに行く</button>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    toUsers() {
+      this.$router.push({ path: "users/1" })
+    }    
+  }
+}
+</script>
